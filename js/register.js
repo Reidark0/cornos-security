e8dd55505db68f3c0b5c30925b674d263a30a5df
Rafael Cesar <rcesar@nuageit.com.br>
2025-03-24 00:00:51 -0300

    fix: add auth() to register.js

--- js/register.js
+++ js/register.js
@@ -23,7 +23,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-
+const auth = getAuth();
 
 // submit
 const submit = document.getElementById('submit');
