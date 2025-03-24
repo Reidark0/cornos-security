9570ddb03e4c5830aba2cb5b10804b9bd70f3458
Rafael Cesar <rcesar@nuageit.com.br>
2025-03-23 23:56:05 -0300

    fix: register account

--- js/register.js
+++ js/register.js
@@ -23,14 +23,17 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-// inputs
-const email = document.getElementById('email').value;
-const password = document.getElementById('password').value;
+
 
 // submit
 const submit = document.getElementById('submit');
 submit.addEventListener('click', function(event){
     event.preventDefault();
+
+    // inputs
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
     createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
         // Signed up 
